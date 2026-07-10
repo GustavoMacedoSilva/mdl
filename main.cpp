53d1febc5af6142f5b804a46e2178ee5b4b390db
GustavoMSilva <macedo.gustavo@ges.inatel.br>
2026-07-10 19:51:59 -0300

    feat: build teste inicial do uso do FTXUI, tamo aprendendo tamo aprendendo

--- main.cpp
+++ main.cpp
@@ -1,19 +1,43 @@
-#include "core/downloader.hpp"
-#include <iostream>
+// #include "core/downloader.hpp"
+#include <ftxui/component/app.hpp>
+#include <ftxui/component/captured_mouse.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/component_base.hpp>
+#include <ftxui/dom/elements.hpp>
 #include <spdlog/spdlog.h>
 #include <string>
 
+using namespace ftxui;
+
+void Video(std::string url) {}
+
+void Audio(std::string url) {}
+
 int main() {
+  App screen = App::FitComponent();
+  std::string url("");
+  std::string url_final;
+  Component input_url = Input(&url, "placeholder");
+  auto video_button = Button("Video", [&url, &screen, &url_final] {
+    url_final = url;
+    screen.PostEvent(Event::Custom);
+  });
 
-  std::string url = "url.test.com";
-  std::string quality = "BEST";
-
-  std::cout << "Hello, Wordl!" << "\n";
-  spdlog::warn("TEST SPDLOG");
-
-  yt::download_audio(url);
-  yt::download_video(url, quality);
-  yt::get_video_info(url);
+  auto component = Container::Vertical({
+      input_url,
+      video_button,
+  });
+  auto renderer = Renderer(component, [&] {
+    return vbox({
+               hbox(text("input url: "), input_url->Render()),
+               hbox(text(""), video_button->Render()),
+               vbox({
+                   text(url_final),
+               }) | border,
+           }) |
+           border;
+  });
+  screen.Loop(renderer);
 
   return 0;
 }
